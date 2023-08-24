bdd6a877abad925228be5bed76ec6d4e7c097280
sincanmaulanaa <sincanmaulanaa@gmail.com>
2023-08-24 15:52:43 +0700

    Initialize case study

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <div className='card-container'>
-        <Todo title='Learn React Section 1' />
-        <Todo title='Learn React Section 2' />
-        <Todo title='Learn React Section 3' />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
